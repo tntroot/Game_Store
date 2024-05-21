56e585cf83260105e71cd196a5ce18d6bb88eb3b
roottnt <cherhorn538@gmail.com>
2024-05-21 16:31:21 +0800

    更新

---
+++ src/components/ItemCard.vue
@@ -0,0 +1,113 @@
+<script>
+import { RouterLink } from 'vue-router';
+import { Icon } from '@iconify/vue';
+export default {
+    props: {
+        itemList: {
+            type: Object,
+        }
+    },
+    components: {
+        Icon,
+    },
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+<template>
+    <div class="card my-5">
+        <div class="row g-0">
+            <div class="col-lg-6 bg-black">
+                <swiper-container class="mySwiper" thumbs-swiper=".mySwiper2" space-between="10" navigation="true">
+                    <swiper-slide v-for="(item, index) in itemList.img" :key="index">
+                        <img class="w-100 md:tw-h-[24rem] tw-h-[12rem]" :src="item" alt="" srcset="">
+                    </swiper-slide>
+                </swiper-container>
+                <swiper-container class="mySwiper2" space-between="10" slides-per-view="3" free-mode="true"
+                    watch-slides-progress="true">
+                    <swiper-slide v-for="(item, index) in itemList.img" :key="index">
+                        <img class="w-100 tw-h-[6rem]" :src="item" alt="" srcset="">
+                    </swiper-slide>
+                </swiper-container>
+            </div>
+            <div class="col-lg-6">
+                <div class="card-body">
+                    <h1 class="card-title">Minecraft</h1>
+                    <table class=" itemTable">
+                        <tr>
+                            <td>作者：</td>
+                            <td>凱薩</td>
+                        </tr>
+                        <tr>
+                            <td>遊戲類型：</td>
+                            <td>角色扮演</td>
+                        </tr>
+                        <tr>
+                            <td>上架時間：</td>
+                            <td><small class="text-muted">2024/05/16</small></td>
+                        </tr>
+                        <tr>
+                            <td>遊戲標籤：</td>
+                            <td>
+                                <div class="d-flex flex-wrap">
+                                    <div
+                                        v-for="(item, index) in ['3D', '可愛', '3D', '可愛', '3D', '可愛', '3D', '可愛', '可愛', '可愛', '可愛', '可愛']">
+                                        <RouterLink to="/">
+                                            <span class=" tw-text-blue-600 hover:tw-text-red-600">{{ item }}</span>
+                                            <span v-if="['3D', '可愛'].length !== index + 1">、</span>
+                                        </RouterLink>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                價格：
+                            </td>
+                            <td>
+                                <div class="d-flex align-items-center">
+                                    <h5 class="card-text text-danger fw-bolder fs-1 me-2 py-3"> {{ 200 != 0 ? `$ ${200}` :
+                                        '免費' }}</h5>
+                                    <h5 v-if="true" class="text-decoration-line-through">${{ 300 }}</h5>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                    <div class="btn btn-lg btn-outline-dark mt-3 fw-bolder mx-auto d-block">
+                        <Icon icon="typcn:shopping-cart" class=" d-inline-block me-2" />
+                        <span>加入購物車</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.mySwiper2 {
+    padding: 0.8rem;
+    background-color: rgba(0, 0, 0, 0.911);
+
+    img {
+        cursor: pointer;
+    }
+}
+
+.card-title {
+    border-bottom: 0.3rem solid #00d9ff;
+    padding-bottom: 1rem;
+}
+
+.itemTable {
+    font-size: 1.5rem;
+    table-layout: fixed;
+    width: 100%;
+
+    tr>td:nth-child(1) {
+        padding: 0.3rem 0;
+        width: 150px;
+    }
+}</style>
--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,13 +1,12 @@
 <script>
-import { RouterLink } from 'vue-router';
-import { Icon } from '@iconify/vue';
+import ItemCard from '../components/ItemCard.vue';
 export default {
 	components:{
-		Icon,
+		ItemCard,
 	},
 	data() {
 		return {
-			itemList: {
+			item: {
 				"id": 1,
 				"name": "Minecraft",
 				"img": ["/src/assets/img/minecraft/minecraft.webp", "/src/assets/img/minecraft/minecraft2.jpg", "/src/assets/img/minecraft/minecraft3.jpg", "/src/assets/img/minecraft/minecraft4.jpg"],
@@ -20,99 +19,26 @@ export default {
 		}
 	},
 	created() {
-		// this.itemList.img.map( item => {item = new URL(item, import.meta.url)});
 	},
 }
 </script>
 
 <template>
 	<div class=" container">
-		<div class="row my-5">
-			<div class="card mb-3">
-				<div class="row g-0">
-					<div class="col-md-6">
-						<swiper-container class="mySwiper" thumbs-swiper=".mySwiper2" space-between="10" navigation="true">
-							<swiper-slide v-for="(item, index) in itemList.img" :key="index">
-								<img class="w-100 lg:tw-h-[24rem] tw-h-[12rem]" :src="item" alt="" srcset="">
-							</swiper-slide>
-						</swiper-container>
-						<swiper-container class="mySwiper2" space-between="10" slides-per-view="3" free-mode="true"
-							watch-slides-progress="true">
-							<swiper-slide v-for="(item, index) in itemList.img" :key="index">
-								<img class="w-100 sm:tw-h-[6rem] tw-h-[3rem]" :src="item" alt="" srcset="">
-							</swiper-slide>
-						</swiper-container>
-					</div>
-					<div class="col-md-6">
-						<div class="card-body">
-							<h1 class="card-title">Minecraft</h1>
-							<table class="card-text itemTable">
-								<tr>
-									<td>作者：</td>
-									<td>凱薩</td>
-								</tr>
-								<tr>
-									<td>遊戲類型：</td>
-									<td>角色扮演</td>
-								</tr>
-								<tr>
-									<td>上架時間：</td>
-									<td><small class="text-muted">2024/05/16</small></td>
-								</tr>
-								<tr>
-									<td>遊戲標籤：</td>
-									<td>
-										<div class="d-flex flex-wrap">
-											<div v-for="(item, index) in ['3D', '可愛']">
-												<RouterLink to="/">
-													<span class=" tw-text-blue-600 hover:tw-text-red-600">{{ item }}</span>
-													<span v-if="['3D', '可愛'].length !== index + 1">、</span>
-												</RouterLink>
-											</div>
-										</div>
-									</td>
-								</tr>
-								<tr>
-									<td>
-										價格：
-									</td>
-									<td>
-										$ 1234
-									</td>
-								</tr>
-							</table>
-							<div class="btn btn-outline-dark mt-3 fw-bolder">
-								<Icon icon="typcn:shopping-cart" class=" d-inline-block me-2" />
-								<span>加入購物車</span>
-							</div>
-						</div>
-					</div>
-				</div>
+		<ItemCard :itemList="item" />
+		<div class="row">
+			<div class="col-12 bg-white rounded-5 p-5">
+				<p class="h2">h1. Bootstrap heading</p>
 			</div>
 		</div>
 	</div>
 </template>
 
 <style lang="scss" scoped>
-.mySwiper2 {
-	padding: 0.8rem;
-	background-color: rgba(0, 0, 0, 0.911);
-
-	img {
-		cursor: pointer;
+	.h2{
+		border-bottom: 0.3rem solid #00d9ff;
+    	padding-bottom: 1rem;
 	}
-}
-.card-title{
-	border-bottom: 0.3rem solid #00d9ff;
-	padding-bottom: 1rem;
-}
-.itemTable{
-	margin: 2.5rem;
-	font-size: 1.5rem;
-	tr > td:nth-child(2){
-		padding: 0.3rem 0 0.3rem 1.5rem;
-	}
-}
 </style>
 
 <!-- https://codesandbox.io/p/sandbox/qd6gyz?file=%2Findex.html%3A171%2C16-171%2C75 -->

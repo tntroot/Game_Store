d66c6b4dc65dff3f8d16fefd7221be9d28c4d603
roottnt <cherhorn538@gmail.com>
2024-06-14 15:47:13 +0800

    更新頁面

--- src/views/Account/CheckoutView.vue
+++ src/views/Account/CheckoutView.vue
@@ -7,8 +7,11 @@
         <div class="row">
             <div class="col-12">
                 <h3 class="tw-border-b-4 rounded-3 border-primary mb-4">
-                    <p class="badge bg-primary rounded-bottom-0 p-3 ms-4 fs-6">結帳</p>
+                    <p class="badge bg-primary rounded-bottom-0 p-3 ms-4 fs-6">購買遊戲</p>
                 </h3>
+                <div>
+                    
+                </div>
             </div>
         </div>
     </div>
